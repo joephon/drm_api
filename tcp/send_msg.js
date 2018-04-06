03d0de2620a33ed46bd4429f61a5d115b70031c3
pennyjs <pennyjs@github.io>
2018-04-06 16:33:47 +0800

    pennyjs <pennyjs@github.io>

--- tcp/send_msg.js
+++ tcp/send_msg.js
@@ -4,16 +4,17 @@ const Auth = require('../model/Auth')
 const Cache = require('../model/Cache')
 const { quotaDic } = require('../utils/dic')
 const SMS = require('../utils/SMS')
+const superagent = require('superagent')
 
 module.exports = async obj => {
   obj.data.map(async item => {
 
     const key = Object.keys(item)[0]
-    const newObj = quotaDic(key)                         
+    const newObj = quotaDic(key)
 
     if (newObj.quotaClass != 3) return
 
-    if (Number(item[key]) != 1) return 
+    if (Number(item[key]) != 1) return
 
     const popu = await Auth.find({canMonitor: true})
                   .populate('user', 'phone')
@@ -30,15 +31,30 @@ module.exports = async obj => {
 
       const cache = await Cache.find(insert).sort({_id: -1})
 
-      const offset = 60 * 60 * 1000
+      // TODO: Add New SMS API
+      const { settings } = item2.device
+      let offset = 60 * 60 * 1000
+      let enable = true
+      for (let setting of settings) {
+        if (setting.key === key) {
+          offset = (~~setting.interval) * 1000
+          enable = (setting.status === 0 ? true : false)
+        }
+      }
 
       if (cache && Date.now() - Number(cache[0].ts) < offset ) return
+      if (! enable) return null
 
       insert.ts = obj.ts
       const cacheStore = await Cache.create(insert)
-      SMS(insert.phone, insert.name, insert.msg)
+      await superagent.post(`http://127.0.0.1:4000/api/devices/${item2.device._id}/warning`).send({
+        phone: insert.phone,
+        name: insert.name,
+        msg: insert.msg
+      }).then(_ => {
+        return null
+      })
+      // SMS(insert.phone, insert.name, insert.msg)
     })
   })
 }
-
-
